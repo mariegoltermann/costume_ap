0b3bfc049af0deb3e6fc877ed78f90a4c2e3c492
mariegoltermann <mari09a7@stud.kea.dk>
2023-05-10 10:30:06 +0200

    heje

--- java.js
+++ java.js
@@ -52,3 +52,19 @@ window.addEventListener("click", function(event) {
     popup.style.display = "none";
   }
 });
+
+/* burgermenu*/
+const burgermenu = document.getElementById("burgermenu");
+const nav = document.getElementById("nav-list");
+
+burgermenu.addEventListener("click", () => {
+  burgermenu.classList.toggle("active");
+
+  if (burgermenu.classList.contains("active")) {
+    burgermenu.textContent = "×";
+    nav.style.display = "flex";
+  } else {
+    burgermenu.textContent = "☰";
+    nav.attributes.removeNamedItem("style");
+  }
+});
